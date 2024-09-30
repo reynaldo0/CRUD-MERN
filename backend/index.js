0549bdbd6c19d866c45c489e8a454b15d8eb57cc
reynaldo0 <reynaldooyusellino@gmail.com>
2024-09-30 10:17:29 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import UserRoute from "./routes/UserRoute"
+
+const app = express();
+mongoose.connect("mongodb://localhost:27017/crud_db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Database Connected.."));
+
+app.use(cors());
+app.use(express.json());
+app.use(UserRoute);
+
+app.listen(5000, () => console.log("Server Up And Running..."));
